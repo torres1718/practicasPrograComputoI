7ae53e085e61e6719b9c8b175f62a21422bbeda2
torres1718 <cristobaltorres273@gmail.com>
2022-02-21 08:35:18 -0600

    Subida de la practica semana 4

---
+++ practicaSemana4/componentes/src/components/CartItems.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="root">
+        <ul v-for="(item, index) in cartItems" v-bind:key="index">
+            <li>{{item.productName}} {{item.quantity}}</li>
+        </ul>   
+
+    </div>
+</template>
+
+<script>
+export default{
+    name:'CartItems',
+    props:['cartItems', 'itemCount'],
+    methods:{
+
+    }
+}
+</script>
